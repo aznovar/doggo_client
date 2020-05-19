0756beca32b65d1529391dbc6689985e2cb9e48b
kkriegerk <kkrieger5@gmail.com>
2020-05-19 23:44:54 +0300

    Добавлена первая версия разметки экрана регистрации: Добавлены активити и фрагмент регистрации Добавлены ассеты на форму

--- app/src/main/java/com/ru/appdoggo/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/ru/appdoggo/ui/register/RegisterActivity.kt
@@ -1,7 +1,13 @@
 package com.ru.appdoggo.ui.register
 
+import android.os.Bundle
+import com.ru.appdoggo.R
 import com.ru.appdoggo.ui.core.BaseActivity
 
-class RegisterActivity : BaseActivity(){
+class RegisterActivity : BaseActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
 }
