73f00f64c5e0f3c7a0aea48e759c2c3f491f74a2
carryweight <stajerlol@gmail.com>
2020-05-24 22:16:46 +0300

    first commit in this branch

---
+++ app/src/main/java/com/ru/appdoggo/ui/commonDogInformation/commonDogInformationActivity.kt
@@ -0,0 +1,13 @@
+package com.ru.appdoggo.ui.commonDogInformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ru.appdoggo.R
+
+class commonDogInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common_dog_information)
+    }
+}
