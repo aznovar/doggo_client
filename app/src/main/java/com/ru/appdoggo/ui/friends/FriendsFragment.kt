cef9a397340824e54a6b14aea55624f296350165
kkriegerk <kkrieger5@gmail.com>
2020-10-12 17:19:21 +0300

    Добавил первый вариант tab layout и view pager для страницы друзей

--- app/src/main/java/com/ru/appdoggo/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/ru/appdoggo/ui/friends/FriendsFragment.kt
@@ -1,10 +1,37 @@
 package com.ru.appdoggo.ui.friends
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.ru.appdoggo.R
 import com.ru.appdoggo.ui.core.BaseFragment
 
-class FriendsFragment : BaseFragment(){
+class FriendsFragment : BaseFragment() {
 
     override val layoutId = R.layout.fragment_friends
     override val titleToolbar = R.string.title_friends
+
+    private lateinit var fragmentSectionPagerAdapter: FriendsFragmentSectionPagerAdapter
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fragmentSectionPagerAdapter = FriendsFragmentSectionPagerAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = fragmentSectionPagerAdapter
+        tabLayout = view.findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+    }
 }
---
+++ app/src/main/java/com/ru/appdoggo/ui/friends/FriendsListFragment.kt
@@ -0,0 +1,29 @@
+package com.ru.appdoggo.ui.friends
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.ru.appdoggo.R
+
+const val ARG_OBJECT = "object"
+
+class FriendsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_item_friends, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(android.R.id.text1)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+}
