b6e13cf2cd1eb81a9c20a3148e77d8abf19b8703
aznovar <kkrieger5@gmail.com>
2021-02-11 15:48:24 +0300

    Добавил инъекцию зависимости для дао сообщений

--- app/src/main/java/com/ru/appdoggo/domain/injectors/CacheModule.kt
+++ app/src/main/java/com/ru/appdoggo/domain/injectors/CacheModule.kt
@@ -5,6 +5,8 @@ import android.content.SharedPreferences
 import com.ru.appdoggo.data.cache.AccountCache
 import com.ru.appdoggo.data.cache.AccountCacheImplementation
 import com.ru.appdoggo.data.cache.SharedPreferencesManager
+import com.ru.appdoggo.data.cache.message.MessagesCache
+import com.ru.appdoggo.data.db.ChatDatabase
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -23,4 +25,9 @@ class CacheModule {
     fun provideAccountCache(prefsManager: SharedPreferencesManager): AccountCache =
         AccountCacheImplementation(prefsManager)
 
+    @Provides
+    @Singleton
+    fun provideMessagesCache(chatDatabase: ChatDatabase): MessagesCache {
+        return chatDatabase.messagesDao
+    }
 }
