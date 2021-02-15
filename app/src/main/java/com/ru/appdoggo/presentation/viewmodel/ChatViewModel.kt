2da21b9396afd616529d37b3c8837e1c77ac0270
aznovar <kkrieger5@gmail.com>
2021-02-15 12:59:37 +0300

    Добавил обработку данных в ChatViewModel.kt

--- app/src/main/java/com/ru/appdoggo/presentation/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/ru/appdoggo/presentation/viewmodel/ChatViewModel.kt
@@ -1,25 +1,41 @@
 package com.ru.appdoggo.presentation.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import com.ru.appdoggo.domain.entities.chat.MessageEntity
 import com.ru.appdoggo.domain.type.None
+import com.ru.appdoggo.domain.usecases.chat.GetChats
 import com.ru.appdoggo.domain.usecases.chat.SendMessage
 import javax.inject.Inject
 
 class ChatViewModel @Inject constructor(
-    private val sendMessageUseCase: SendMessage
+    val getChatsUseCase: GetChats,
+    val sendMessageUseCase: SendMessage
 ) : BaseViewModel() {
 
     val sendMessageData: MutableLiveData<None> = MutableLiveData()
+    val getChatsData: MutableLiveData<List<MessageEntity>> = MutableLiveData()
 
+    fun getChats(needFetch: Boolean = false) {
+        getChatsUseCase(GetChats.Params(needFetch)) { it.either(::handleFailure) { handleGetChats(it, !needFetch)} }
+    }
 
     fun sendMessage(toId: Long, message: String) {
         sendMessageUseCase(SendMessage.Params(toId, message))
         { it.either(::handleFailure) { handleSendMessage(it, toId) } }
     }
 
-
     private fun handleSendMessage(none: None, contactId: Long) {
         sendMessageData.value = none
         // todo add getMessages(contactId, true)
+    }
+
+    private fun handleGetChats(messages: List<MessageEntity>, fromCache: Boolean){
+        getChatsData.value = messages
+        updateProgress(false)
+
+        if (fromCache) {
+            updateProgress(true)
+            getChats(true)
+        }
     }
 }
